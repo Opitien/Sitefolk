d00634496b2f03a76c482e615c423441870e487e
Opitien Ejiroghene <opitiene@gmail.com>
2026-09-19 07:01:50 +0300

    tag manager installed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -156,8 +156,33 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
             gtag('config', 'G-5RYXZKYC80');
           `}
         </Script>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-N8PS36DC');
+          `}
+        </Script>
+        <Script id="apollo-tracking" strategy="afterInteractive">
+          {`
+            function initApollo(){var n=Math.random().toString(36).substring(7),o=document.createElement("script");
+            o.src="https://assets.apollo.io/micro/website-tracker/tracker.iife.js?nocache="+n,o.async=!0,o.defer=!0,
+            o.onload=function(){window.trackingFunctions.onLoad({appId:"6aa67bdce8689d00102aeeb1"})},
+            document.head.appendChild(o)}initApollo();
+          `}
+        </Script>
       </head>
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-N8PS36DC"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         {children}
         <CookieBanner />
       </body>
